58344eff9e180d00507ffa0225a8be9c6522bee7
andalib <andalib.iftakher@gmail.com>
2025-03-30 00:59:01 +0600

    fixed for  different pc

--- app/database.py
+++ app/database.py
@@ -1,9 +1,13 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import urllib.parse
 
 
-DATABASE_URL = "postgresql+asyncpg://postgres:1234][';@localhost:2210/quizwizz"
+
+DATABASE_URL = "postgresql+asyncpg://anda:andalib@localhost:2210/quizwizz"
+
+
 
 
 engine = create_async_engine(DATABASE_URL, echo=True)
